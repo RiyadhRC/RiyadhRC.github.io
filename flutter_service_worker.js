55bb5d94a043d505a082fe24c513ca64750bc3c2
jadenfreude <jadenfreude@users.noreply.github.com>
2020-10-15 00:30:24 +0000

    deploy: 5dd2e455090ff740320b7e664cebd50414074850

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -12,7 +12,7 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "5ec9ef63d1b15bd6ec5538d088c3005b",
 "audio/01-create-eng.mp3": "83d1e1f303e4b8f4cee1cadd9ad75555",
-"feed.xml": "b36515cc44ec0ee8a3245c0ef60b9a8e",
+"feed.xml": "a60c3b2c896f1e1b60bd0bc28b8fbfd6",
 "img/icon.jpg": "900666dc913f9acaa2d76127a2972814",
 "img/spotify.png": "0a5ef7a942cb1d9a64169f57ba1a05c3",
 "main.dart.js": "fa55d961221f15f3e80c53f9e52ccdab",
